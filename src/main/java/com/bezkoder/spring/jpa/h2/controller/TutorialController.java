aebaff6fcf7f6f54133803b8b4db17585fc45391
srikkanth ramesh <sramesh@salesforce.com>
2023-02-02 14:43:08 -0800

    first push

--- src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/controller/TutorialController.java
@@ -21,13 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.jpa.h2.model.Tutorial;
 import com.bezkoder.spring.jpa.h2.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
 
 	@Autowired
 	TutorialRepository tutorialRepository;
+
+	@GetMapping("/hello")	
+    public String hello() {
+        return "Hello, World!";
+    }
 
 	@GetMapping("/tutorials")
 	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
